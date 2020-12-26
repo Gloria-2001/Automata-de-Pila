290b6d87ccd7b12d2681fa1b0898eb988ed08dd1
Gloria-2001 <olimen141@gmail.com>
2020-12-26 17:56:06 -0600

    Automata Pila y Configuraciones

--- EvalAP.java
+++ EvalAP.java
@@ -1,169 +1,190 @@
+/*
+    Instituto Politécnico Nacional 
+    Escuela Superior de Cómputo 
+    Teoria Computacional. 2CV1
+    Profr. Benjamin Luna Benoso 
+    Gloria Oliva Olivares Menez 
+    26/12/2020
+
+    Funcionamiento: Las configuraciones están en el archivo .rar con los nombres de C1.txt, C2.txt y C3.txt 
+                    Para compilar el programa, se hace a través de la terminal (Linux) o del cmd (Windows).
+                    Primero se coloca el comando javac seguido del nombre del archivo con la extension
+                    .java, por tanto, quedaria de la siguiente forma: javac EvalAP.java 
+                    Posteriormente, para ejecutarlo, se coloca el comando java, el nombre del archivo (sin la 
+                    extension) y finalmente el nombre del archivo con la configuracion. Es decir: 
+                    java EvalAP C1.txt
+
+    Configuraciones: En los respectivos archivos txt viene el lenguaje que acepta cada una de las configuraciones.
+*/
+
 import java.util.*;
 import java.io.*;
 
 public class EvalAP{
 
-    public int columnaF;  //numero de columnas
-    private String estadoInicial, estadoFinal, estadoActual;
-    private ArrayList<String[]> trans;
-    private HashMap<String, HashMap<String,String[]>> tabla; 
-    private HashMap<String, String[]> tablaAux; 
-    private String[] simIni; 
-    private Stack<String> pila; 
-    public String cadena;
+    private String symbols[];   //array para guardar el alfabeto
+    private String lenguaje[];  //array para guardar el lenguaje 
+    private String ini, edoActual;
+    private String fin[];
+    private Stack<String> pila;   //declaración de la pila 
+    private HashMap<String,HashMap<String,String[]>> table; 
+    private String movimiento = "q0->"; //declaración del movimiento de la pila 
 
-    public EvalAP(String config){
-        tabla=new HashMap<String,HashMap<String,String[]>>();
-        tablaAux=new  HashMap<String, String[]>();
-
+    public EvalAP(String name){
+        this.table = new HashMap<String,HashMap<String,String[]>>(); //se instancia el hashmap
+        pila = new Stack<String>();  //se instancia la pila
+        pila.push("Z");     //se agrega la base de la pila
         try{
-            //Creacion y apertura del fichero
-            File inputFile = new File(config);
+            // Se crea y se abre un fichero.
+            File inputFile = new File(name);
 
-            //Lector de archivo y buffer para el contenido del mismo
+            // Se crea un lector del archivo y un buffer
+            // que contendrá el texto del archivo
             FileReader fr = new FileReader(inputFile);
             BufferedReader br = new BufferedReader(fr);
 
-            //Leer linea por linea
+            // Se lee linea por linea el archivo
             String linea;
-            String [] aux; 
-            int i=0;
+            String[] aux;
             linea = br.readLine();
-            while(linea!=null){
-                //Se hace la division para los estados
-                String dato[]= linea.split(":|\\,");
-                if(i==0){
-                    //Si la línea empieza con @ 
-                    if(linea.charAt(0)=='@'){
-                        this.columnaF = dato.length;    //el No. de Columnas será la longitud del arreglo de datos
-                        String[] aux3 = Arrays.toString(trans);
-                        aux3 = Arrays.copyOfRange(dato,1,this.columnaF); //los simbolos seran los datos después de la posición 0 (@)
-                    }else{  //Si no hay @ el archivo no es válido
-                    System.out.println("Archivo no valido");
-                    System.exit(0);
-                    }
-                }else if(dato[0].equals("Inicio")){ //Si la posicion en 0 de datos es igual a la palabra inicio
-                    estadoInicial = dato[1]; //es el estado inicial
-                    estadoActual = dato[1];
-                }else if(dato[0].equals("Final")){ //Si la posicion en 0 de datos es igual a la palabra final
-                    estadoFinal = dato[1]; //Es el estado final 
+            while(linea != null){
+                // Se divide la cadena y se guarda en un array
+                String dato[] = linea.split(":"); //los edos se dividen por 2 puntos
+                if(linea.charAt(0) == '@'){
+                    symbols = dato[1].split(",");  //los datos de dividen por comas 
+                    lenguaje = dato[2].split(":");
+                }else if(dato[0].equals("Inicio")){
+                    ini = dato[1];
+                    edoActual = dato[1];
+                }else if(dato[0].equals("Final")){
+                    fin = Arrays.copyOfRange(dato, 1, dato.length);
                 }else{
-                    /*de otra forma, los datos se guardaran en la variable auxiliar
-                    y se pondrán en el hashmap auxiliar, luego lo que esté en el hash 
-                    auxiliar se pondrá en el hashmap original
-                    */
-                    aux = Arrays.copyOfRange(dato,1,this.columnaF);  
-                    tablaAux.put(dato[1],aux);                   
-                    tabla.put(dato[0],tablaAux);
-                    simIni=tablaAux.get(dato[2]);
+                    HashMap<String,String[]> tabAux = new HashMap<String,String[]>(); //se instancia el hashmap auxiliar 
+                    aux = Arrays.copyOfRange(dato, 1, dato.length);  
+                    for(String transi : aux){
+                        String []daux = transi.split(",");
+                        tabAux.put(daux[0],Arrays.copyOfRange(daux, 1, daux.length));  //se guardan los datos en el hash auxiliar 
+                    }
+                    table.put(dato[0], tabAux); //se guardan los datos en el hash principal 
                 }
+                // System.out.println("Linea "+ i +": "+linea);
                 linea = br.readLine();
-                i++;
             }
-            tabla.remove(""); //Eliminar espacio en blanco 
-            fr.close(); //Cerrar lector
+        
+            table.remove("");   // Elimino espacio en blanco
+
+            // Se cierra el lector del archivo
+            fr.close();
+
         }catch(FileNotFoundException e){
-            System.err.println("ArchivoText: "+e);
+            System.err.println("ArchivoText: " + e);
             System.exit(0);
         }catch(IOException e){
-            System.err.println("ArchivoText: "+e);
+            System.err.println("ArchivoText: " + e);
             System.exit(0);
         }
     }
 
-    public void analizarCadena(){
-        Scanner cad = new Scanner(System.in);
-        System.out.print("Ingrese la cadena: ");
-        cadena = cad.nextLine();
-        for(int i=0; i<cadena.length(); i++){        // va a leer la cadena elemento por elemento 
-            int indAux = -1;                         //se establece un indice auxiliar 
-            for(int j=0; j<columnaF-1; j++){        //Va a pasar por cada columna e imprimira los caracteres 
-                if(cadena.charAt(i) == this.trans[j].charAt(0)){
-                    indAux = j;
-                    break;
+    public void showData(){
+        System.out.println("Alfabeto: " +Arrays.toString(symbols));  // se muestra el alfabeto
+        System.out.println("Lenguaje: " +Arrays.toString(lenguaje));  //se muestra el lenguaje 
+        /*                                                              //Todo el código comentado es para que se muestren los estados y las transiciones 
+        for (String i : table.keySet()) {
+            System.out.println("Estado: " + i + "; Transiciones:");
+            HashMap<String,String[]> aux = table.get(i);
+            for(String j : aux.keySet()){
+                System.out.print("\t" + j + ":");
+                String []tranAux = aux.get(j);
+                for(String k : tranAux){
+                    System.out.print(k+", ");
                 }
+                System.out.println();
             }
-            if(indAux != -1){
-                String[] auxEstado = this.tablaAux.get(this.estadoActual);
-                this.transicionAP(auxEstado,indAux);
-            }else{
-                System.out.println("\nNo pertenece al Alfabeto Establecido"); // Indica que no pertenece al alfabeto establecido
-                System.exit(0);
-            }
+            System.out.println();
         }
-        pila.clear();
-        String aux2 = simIni[0];
-        pila.push(aux2);
-        estadoActual=estadoInicial;
-        boolean transiciones = true;
-        int NoIteraciones = cadena.length();
-        for(int i=0; i<NoIteraciones; i++){
-            if(transiciones==true){
-                String simbActual = cadena.substring(0,1);
-                cadena=cadena.substring(1);
-                transiciones=false;
 
-                for(int j=0; j<trans.size(); j++){
-                    if(estadoActual.equals(trans.get(j)[0]) && simbActual.equals(trans.get(j)[1]) && pila.peek().equals(trans.get(j)[2])){
-                        estadoActual = trans.get(j)[3];
-                        pila.pop();
-
-                        if(trans.get(j)[4].length() > 1){
-                            String aux = trans.get(j)[4];
-                            for(int k=0; k < aux.length; k++){
-                                String aux1= aux.substring(aux.length()-1);
-                                aux = aux.substring(0,aux.length()-1);
-                                pila.push(aux1);
-                            }
-                        }else{
-                            if(trans.get(j)[4].equals("@")){
-                                //nada 
-                            }else{
-                                pila.push(trans.get(j)[4]);
-                            }
-                        }
-                        transiciones=true;
-                        break;
-                    }
-                }
-            }else{
-                break;
-            }
-        }
-        cadenaSi(cadena);
-    }
-
-    public void cadenaSi(String cadena){
-        if(cadena.isEmpty() && pila.isEmpty()){
-            System.out.println("Cadena aceptada");
-        }else{
-            System.out.println("Cadena No Aceptada");
-        }
-        if(cadena.isEmpty() && estadoActual.equals(estadoFinal)){
-            System.out.println("Cadena aceptada");
-            break;
-        }else
-            System.out.println("Cadena No Aceptada");
-    }
-
-    public void transicionAP(String[] estados,int ind){//Metodo transiciones
-        this.estadoActual = estados[ind];
-    }
-
-    public void mostrarDato() { // Metodo que muestra los datos de la configuracion 
-        String[] aux;
-        System.out.print("\nAlfabeto aceptado: ");
-        for(int i=0;i<this.columnaF-1;i++){
-            System.out.print(trans[i]+" ");   //muestra el alfabeto aceptado
+        System.out.println("Estado inicial: "+ini);
+        System.out.print("Estados finales: ");
+        for(int i=0;i<this.fin.length;i++){
+            System.out.print(fin[i]+",");
         }
         System.out.println();
-        System.out.println("Estado inicial: "+estadoInicial);
-        System.out.println("Estado final: "+estadoFinal);
+        */
     }
 
-    public static void main(String[] args) {
+    //se analiza la cadena 
+    public void analizarCadena(){
+        String cadena; 
+        Scanner cad = new Scanner(System.in);
+        System.out.print("Ingrese la cadena: ");   //la cadena la ingresa el usuario 
+        cadena = cad.nextLine();
+        cadena = cadena+"@";
+        int tam=cadena.length();
+        String cadAux = cadena;        // se declara una cadena auxiliar, que será la misma cadena pero cada vez mas reducida  
+        System.out.println(cadAux); 
+       for(int i=0;i<tam;i++){
+           char cc = cadena.charAt(i);
+            cadAux = cadAux.substring(1);
+            System.out.println(cadAux); 
+            if(alfabeto(cc) || cc=='@'){  //si la cadena tiene un caracter @ o el metodo alfabeto es true 
+                acciones(cc); //se corre el metodo de acciones 
+            }else{
+                System.out.println("No pertenece al alfabeto");  //De otra forma, como no pertenence al alfabeto, termina el programa
+                System.out.println(movimiento); 
+                System.exit(0);
+             }
+        }
+       System.out.println(movimiento);     //se muestra el movimiento que hace la pila 
+       System.out.println("Cadena no valida");  //si se sale del ciclo la cadena no es valida 
+    }
+    
+    public boolean alfabeto(char c){   //este metodo indica si la cadena pertenece al alfabeto 
+        int tam=symbols.length;
+        for(int i=0;i<tam;i++){      //se recorre el arreglo de symbols  
+            if(String.valueOf(c).equals(symbols[i])){   //si el caracter ingresado de la cadena corresponde a alguno de symbols
+                return true;  //es verdadero 
+            }
+        }
+        return false;  //de otra forma es falso 
+    }
+
+    public void acciones(char b){  //este metodo hace las acciones de la pila 
+        String sb = String.valueOf(b);  //se convierte en string el caracter de la cadena de entrada 
+        try{
+            HashMap<String,String[]> aux = table.get(this.edoActual);   //se obtiene el estado actual 
+            String[] aux1 = aux.get(sb);  //se pone en un auxiliar las pequeñas cadenas 
+            if(!aux1[1].equals("@")){ //si esta en la primera posicion va a sacarse de la pila 
+                sb=pila.pop();
+            }
+            if(!aux1[2].equals("@")){   //si esta en la segunda posicion se mete a la pila 
+                pila.push(aux1[2]);
+            }
+            this.edoActual = aux1[0];
+            movimiento+=(Arrays.toString(aux1)+"->");  //se va a ir haciendo el movimiento de la pila 
+            if(sb.equals("Z") && eval()){    //si al final la cadena se iguala a Z (base de la pila) y cumple el metodo eval(), la cadena es aceptada
+                System.out.println(movimiento+"Fin");      
+                System.out.println("Cadena aceptada");
+                System.exit(0);
+            }
+        }catch(Exception e){
+            System.out.println(movimiento);
+            System.out.println("Cadena no valida"); //De otra forma se muestra el movimiento y se rechaza la cadena 
+            System.exit(0);
+        }
+    }
+
+    public boolean eval(){
+        for(int i=0;i<fin.length;i++){   //Si el estado actual de la cadena es igual al estado final, la cadena es compatible
+            if(this.edoActual.equals(this.fin[i])){
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    public static void main(String[] args) { //se implementan los metodos 
         EvalAP ap = new EvalAP(args[0]);
-        ap.mostrarDato();
+        ap.showData();
         ap.analizarCadena();
     }
 }
